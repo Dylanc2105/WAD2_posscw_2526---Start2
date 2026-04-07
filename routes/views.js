754f68c44fc2640038832335eac1baeabc5d6c27
Dylan <Dylanc21@outlook.com>
2026-04-07 04:15:43 +0100

    protect booking routes - require login to book

--- routes/views.js
+++ routes/views.js
@@ -9,14 +9,15 @@ import {
 } from "../controllers/viewsController.js";
 
 import { coursesListPage } from "../controllers/coursesListController.js";
+import { requireLogin } from "../middlewares/requireAuth.js";
 
 const router = Router();
 
 router.get("/", homePage);
 router.get("/courses", coursesListPage);
 router.get("/courses/:id", courseDetailPage);
-router.post("/courses/:id/book", postBookCourse);
-router.post("/sessions/:id/book", postBookSession);
+router.post("/courses/:id/book", requireLogin, postBookCourse);
+router.post("/sessions/:id/book", requireLogin, postBookSession);
 router.get("/bookings/:bookingId", bookingConfirmationPage);
 
 export default router;
